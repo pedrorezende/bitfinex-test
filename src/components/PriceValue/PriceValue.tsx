dec1ed0480f50a9e5469e0c09e9c0786acba6905
Pedro Rezende <xpedrorezende@gmail.com>
2022-11-04 18:05:07 -0300

    refactor: removing unused component

--- src/components/PriceValue/PriceValue.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export interface PriceValueProps {
-  prop?: string;
-}
-
-export function PriceValue({ prop = "default value" }: PriceValueProps) {
-  return <div>PriceValue {prop}</div>;
-}
